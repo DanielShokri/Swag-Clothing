48ad35ace6c5da9a29e425e3a5786f2bf6fd1435
Daniel Shokri <danielshokri@gmail.com>
2019-09-15 16:47:30 +0300

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage.cmp';
-import ShopPage from './pages/ShopPage/ShopPage.cmp'
+import ShopPage from './pages/ShopPage/ShopPage.cmp';
+import AppHeader from './components/Header/AppHeader.cmp';
 
 
 function App() {
   return (
-    <>
+    <div class="container">
+      <AppHeader />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
       </Switch>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/Header/AppHeader.cmp.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/logo.png';
+import './appheader.styles.scss';
+
+const AppHeader = () => {
+
+
+    return (
+        <div>
+            <nav className="navbar" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <Link className="logo" to="/">
+                        <img src={Logo} width="112" height="28" />
+                    </Link>
+
+                    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" className="navbar-menu">
+                    <div className="navbar-end">
+                        <Link className="navbar-item" to="/shop">
+                            <div className="buttons">
+                                SHOP
+                    </div>
+                        </Link>
+                    </div>
+                    <div className="navbar-end">
+                        <Link className="navbar-item" to="/shop">
+                            CONTACT
+      </Link>
+                    </div>
+                    <div className="navbar-end">
+                        <Link className="navbar-item" to="/shop">
+                            CART
+      </Link>
+                    </div>
+
+
+                    <div className="navbar-end">
+                        <div className="navbar-item">
+                            <div className="buttons">
+
+                                <a className="button is-light">
+                                    Log in
+          </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default AppHeader;
